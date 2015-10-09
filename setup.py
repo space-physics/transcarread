e698504780961f24d7aa16a9b2117cb4a2942ae8
scienceopen <scienceopen@users.noreply.github.com>
2015-10-09 11:55:32 -0400

    doc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup 
 
-with open('README.rst') as f:
+with open('README.rst','r') as f:
 	long_description = f.read()
 	
 setup(name='transcarread',
@@ -10,7 +10,6 @@ setup(name='transcarread',
 	  description='Reading utilities for output of TRANSCAR 1-D aeronomy model',
 	  long_description=long_description,
 	  author='Michael Hirsch',
-	  author_email='hirsch617@gmail.com',
 	  url='https://github.com/scienceopen/transcarread',
 	  install_requires=['numpy','pytz','pandas','scipy'],
       packages=['transcarread'],
