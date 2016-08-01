5cbccc68f8b524f222b5d9c5ba8be773da76df65
Michael Hirsch <scienceopen@users.noreply.github.com>
2016-08-01 16:10:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
-import os,sys
 import subprocess
 from setuptools import setup 
 
-exepath = os.path.dirname(sys.executable)
 try:
-    subprocess.call([os.path.join(exepath,'conda'),'install','--yes','--file','requirements.txt'])
+    subprocess.call('conda','install','--file','requirements.txt'])
 except Exception as e:
-    print('tried conda in {}, but you will need to install packages in requirements.txt  {}'.format(exepath,e))
+    pass
 
 
-with open('README.rst','r') as f:
-	long_description = f.read()
 
 setup(name='transcarread',
       packages=['transcarread'],
 	  description='Reading utilities for output of TRANSCAR 1-D aeronomy model',
-	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/transcarread',
 	  install_requires=['gridaurora'],
