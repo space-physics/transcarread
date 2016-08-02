9ede6d159e7bd24352ea28c1db769c0173a9a389
scienceopen <scienceopen@users.noreply.github.com>
2016-08-02 11:51:30 -0400

    prereq

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 from setuptools import setup
 
 try:
-    subprocess.call('conda','install','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
     pass
 
@@ -14,7 +14,8 @@ setup(name='transcarread',
 	  description='Reading utilities for output of TRANSCAR 1-D aeronomy model',
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/transcarread',
-	  install_requires=['gridaurora','histutils'],
+	  install_requires=['pathlib2',
+                        'gridaurora','histutils'],
       dependency_links = [
       'https://github.com/scienceopen/gridaurora/tarball/master#egg=gridaurora',
       'https://github.com/scienceopen/histutils/tarball/master#egg=histutils'],
