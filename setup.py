182d5ae0e5ca700499a35674b56b953916a07610
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-12 18:08:34 -0400

    template

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-req = ['nose','python-dateutil', 'pytz','numpy','xarray', 'scipy',
+install_requires = ['python-dateutil', 'pytz','numpy','xarray', 'scipy',
        'gridaurora','sciencedates']
+tests_require=['pytest','nose','coveralls']
 # %%
 from setuptools import setup,find_packages
 
@@ -8,14 +9,16 @@ setup(name='transcarread',
       packages=find_packages(),
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/transcarrread',
-      version='1.0.0',
+      version='1.0.1',
 	  classifiers=[
       'Intended Audience :: Science/Research',
       'Development Status :: 4 - Beta',
       'Topic :: Scientific/Engineering :: Atmospheric Science',
       'Programming Language :: Python :: 3',
 	  ],
-	  install_requires=req,
+	  install_requires=install_requires,
+	  tests_require=tests_require,
 	  python_requires='>=3.6',
-      extras_require={'plot':['matplotlib','seaborn'],},
+      extras_require={'plot':['matplotlib','seaborn'],
+                      'tests':tests_require,},
 	  )
