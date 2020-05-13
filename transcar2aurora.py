75c71248c5d39931b90b0e32616f78670262a5f1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-05-13 15:49:12 -0400

    iterate and plot beam dirs from root dir

--- transcar2aurora.py
+++ transcar2aurora.py
@@ -4,6 +4,7 @@ Show auroral output, optionally with simulated optical filter.
 
 python transcar2aurora.py tests/data/
 """
+from pathlib import Path
 from argparse import ArgumentParser
 from dateutil.parser import parse
 import transcarread as tr
@@ -17,24 +18,28 @@ except ImportError:
 def main():
     p = ArgumentParser(description="Makes auroral emissions based on transcar sim")
     p.add_argument("path", help="root path that beam directories live in")
-    p.add_argument("-t", "--treq", help="date/time  YYYY-MM-DDTHH-MM-SS", default="2013-03-31T09:00:30Z")
+    p.add_argument("-t", "--treq", help="date/time  YYYY-MM-DDTHH-MM-SS", default="2013-03-31T09:00:30")
     p.add_argument("--filter", help="optical filter choices: bg3   none", default="bg3")
     p.add_argument("--tcopath", help="set path from which to read transcar output files", default="dir.output")
     p = p.parse_args()
 
-    sim = tr.SimpleSim(p.filter, p.tcopath, transcarutc=p.treq)
-    # %% run sim
-    rates = tr.calcVERtc(p.path, parse(p.treq), sim)
-    print(rates)
+    rodir = Path(p.path).expanduser().resolve()
 
-    if figure is not None:
-        ax = figure().gca()
-        ax.semilogx(rates[0, :, :], rates.alt_km)
-        ax.set_ylabel("altitude [km]")
-        ax.set_xlabel("excitation")
-        ax.set_title(str(p.path))
+    dirs = (d for d in rodir.glob('beam*') if d.is_dir())
+    for d in dirs:
+        sim = tr.SimpleSim(p.filter, p.tcopath, transcarutc=p.treq)
+        # %% run sim
+        rates = tr.calcVERtc(d, parse(p.treq), sim.transcarconfig)
+        print(rates)
 
-        show()
+        if figure is not None:
+            ax = figure().gca()
+            ax.semilogx(rates[0, :, :], rates.alt_km)
+            ax.set_ylabel("altitude [km]")
+            ax.set_xlabel("excitation")
+            ax.set_title(d.name)
+
+    show()
 
 
 if __name__ == "__main__":
