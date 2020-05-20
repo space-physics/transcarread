765d6622fb20a2088d1dc9de20b032793d957501
Michael Hirsch <scivision@users.noreply.github.com>
2020-05-20 05:17:16 -0400

    add diff_state.py

--- diff_state.py
+++ diff_state.py
@@ -15,28 +15,31 @@ import transcarread.plots as plots
 import transcarread as tr
 
 
-def compute(path: Path, tReq: datetime, plot_params: list, verbose: bool):
+def compute(path: Path, tReq: datetime, plot_params: list):
     path = Path(path).expanduser().resolve()
     # %% get sim parameters
     datfn = path / "dir.input/DATCAR"
     tctime = tr.readTranscarInput(datfn)
     # %% load transcar output
     iono = tr.read_tra(path, tReq)
-    # %% do plot
-    plots.plot_isr(iono, path, tctime, plot_params, verbose)
 
     return iono, tctime
 
 
 def main():
     p = ArgumentParser(description="reads dir.output/transcar_output")
-    p.add_argument("path", help="path containing dir.output/transcar_output file")
+    p.add_argument("ref", help="old reference path above dir.output/")
+    p.add_argument("new", help="path above new dir.output/")
     p.add_argument("--tReq", help="time to extract data at")
-    p.add_argument("-v", "--verbose", help="more plots", action="store_true")
     p.add_argument("-p", "--params", help="only plot these params", choices=["ne", "vi", "Ti", "Te"], nargs="+")
     p = p.parse_args()
 
-    compute(p.path, p.tReq, p.params, p.verbose)
+    dat1, t1 = compute(p.ref, p.tReq, p.params)
+    dat2, t2 = compute(p.new, p.tReq, p.params)
+
+    diff = dat1-dat2
+    # %% do plot
+    plots.plot_isr(diff, p.new, t2, p.params)
 
     show()
 
